5a4c626b2d548bb1578fc23eb33ff11e787ce08b
BayajidAlam <bayajidalam2001@gmail.com>
2023-06-20 12:56:45 +0600

    prettier and lint-staged setup done

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app: Application = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res: any) => {
+app.get('/', (req, res) => {
   res.send('Api working successfully')
 })
 
