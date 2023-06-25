4f029eb493bab0bcee341b421bcfa16fc7241184
BayajidAlam <bayajidalam2001@gmail.com>
2023-06-25 20:09:37 +0600

    error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
-import express, { Application, Request, Response } from 'express'
+import express, {
+  Application,
+  NextFunction,
+  Request,
+  Response,
+  request,
+  response,
+} from 'express'
 import cors from 'cors'
 import routes from './app/modules/user/user.route'
 const app: Application = express()
@@ -10,9 +17,33 @@ app.use(express.urlencoded({ extended: true }))
 // Application routes
 app.use('/api/v1/users/', routes)
 
+class ApiError extends Error {
+  statusCode: number
+
+  constructor(statusCode: number, message: string | undefined, stack?: '') {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
 // testing api
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Api working successfully')
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  throw new Error('Ora baba error')
+  // next('not implemented')
+})
+
+// global error handler
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err })
+  } else {
+    res.status(500).json({ error: 'Something went wrong' })
+  }
 })
 
 export default app
